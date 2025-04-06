f3ed18b791d5dc170f2fdebb0c331065d0c09666
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2025-04-06 02:29:59 +0200

    test branch protection

--- src/main/java/jabberpoint/SlideViewerComponent.java
+++ src/main/java/jabberpoint/SlideViewerComponent.java
@@ -1,32 +1,19 @@
 package jabberpoint;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-
-/** <p>SlideViewerComponent is een grafische component die Slides kan laten zien.</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
+/**
+ * SlideViewerComponent is a graphical component to show Slides.
  */
-
 public class SlideViewerComponent extends JComponent {
-		
-	private Slide slide; // de huidige slide
-	private Font labelFont = null; // het font voor labels
-	private Presentation presentation = null; // de presentatie
-	private JFrame frame = null;
-	
 	private static final long serialVersionUID = 227L;
-	
+
 	private static final Color BGCOLOR = Color.white;
 	private static final Color COLOR = Color.black;
 	private static final String FONTNAME = "Dialog";
@@ -35,8 +22,13 @@ public class SlideViewerComponent extends JComponent {
 	private static final int XPOS = 1100;
 	private static final int YPOS = 20;
 
+	private Slide slide; // current slide
+	private Font labelFont = null; // font for labels
+	private Presentation presentation = null; // the presentation
+	private JFrame frame = null;
+
 	public SlideViewerComponent(Presentation pres, JFrame frame) {
-		setBackground(BGCOLOR); 
+		setBackground(BGCOLOR);
 		presentation = pres;
 		labelFont = new Font(FONTNAME, FONTSTYLE, FONTHEIGHT);
 		this.frame = frame;
@@ -57,7 +49,6 @@ public class SlideViewerComponent extends JComponent {
 		frame.setTitle(presentation.getTitle());
 	}
 
-// teken de slide
 	public void paintComponent(Graphics g) {
 		g.setColor(BGCOLOR);
 		g.fillRect(0, 0, getSize().width, getSize().height);
@@ -66,9 +57,7 @@ public class SlideViewerComponent extends JComponent {
 		}
 		g.setFont(labelFont);
 		g.setColor(COLOR);
-		g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " +
-                 presentation.getSize(), XPOS, YPOS);
-		Rectangle area = new Rectangle(0, YPOS, getWidth(), (getHeight() - YPOS));
-		slide.draw(g, area, this);
+		g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), XPOS, YPOS);
+		slide.draw(g, this, 0, YPOS, 1.0f);
 	}
 }
