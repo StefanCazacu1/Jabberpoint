5c6ab608492d070275526c7a1ff101475de93a6a
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2025-06-06 22:26:15 +0200

    Fix: use standard switch-case in KeyController for Checkstyle

--- src/main/java/jabberpoint/KeyController.java
+++ src/main/java/jabberpoint/KeyController.java
@@ -13,13 +13,25 @@ public class KeyController extends KeyAdapter {
 	@Override
 	public void keyPressed(KeyEvent keyEvent) {
 		switch (keyEvent.getKeyCode()) {
-			case KeyEvent.VK_PAGE_DOWN, KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT -> presentation.nextSlide();
-			case KeyEvent.VK_PAGE_UP, KeyEvent.VK_UP, KeyEvent.VK_LEFT -> presentation.prevSlide();
-			case KeyEvent.VK_Q -> System.exit(0);
-			case KeyEvent.VK_HOME -> presentation.setSlideNumber(0);
-
-			default -> {
-				/* Do nothing */ }
+			case KeyEvent.VK_PAGE_DOWN:
+			case KeyEvent.VK_DOWN:
+			case KeyEvent.VK_RIGHT:
+				presentation.nextSlide();
+				break;
+			case KeyEvent.VK_PAGE_UP:
+			case KeyEvent.VK_UP:
+			case KeyEvent.VK_LEFT:
+				presentation.prevSlide();
+				break;
+			case KeyEvent.VK_Q:
+				System.exit(0);
+				break;
+			case KeyEvent.VK_HOME:
+				presentation.setSlideNumber(0);
+				break;
+			default:
+				// Do nothing
 		}
 	}
+
 }
