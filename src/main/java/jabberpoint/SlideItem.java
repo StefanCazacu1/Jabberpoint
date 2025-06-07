27b62e8c83578ef1e8ff261b3cadc6ad9b188de6
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2025-06-07 06:02:04 +0200

    fixed slideitem violations

--- src/main/java/jabberpoint/SlideItem.java
+++ src/main/java/jabberpoint/SlideItem.java
@@ -10,71 +10,71 @@ import java.awt.image.ImageObserver;
  */
 public abstract class SlideItem {
 
-	/** The hierarchical level of the item. */
-	private int level;
+    /** The hierarchical level of the item. */
+    private int level;
 
-	/** The color of the item. */
-	private Color color = Color.BLACK;
+    /** The color of the item. */
+    private Color color = Color.BLACK;
 
-	/**
-	 * Constructs a SlideItem.
-	 * @param level the slide item level
-	 */
-	public SlideItem(final int level) {
-		this.level = level;
-	}
+    /**
+     * Constructs a SlideItem.
+     * @param levelParam the slide item level
+     */
+    public SlideItem(final int levelParam) {
+        this.level = levelParam;
+    }
 
-	/**
-	 * Gets the item's level.
-	 * @return the level
-	 */
-	public int getLevel() {
-		return level;
-	}
+    /**
+     * Gets the item's level.
+     * @return the level
+     */
+    public int getLevel() {
+        return level;
+    }
 
-	/**
-	 * Sets the item's level.
-	 * @param lev the new level
-	 */
-	public void setLevel(final int lev) {
-		this.level = lev;
-	}
+    /**
+     * Sets the item's level.
+     * @param lev the new level
+     */
+    public void setLevel(final int lev) {
+        this.level = lev;
+    }
 
-	/**
-	 * Draws the item on the slide.
-	 * @param g the Graphics context
-	 * @param observer the image observer
-	 * @param x the x coordinate
-	 * @param y the y coordinate
-	 * @param scale the scale factor
-	 */
-	public abstract void draw(Graphics g, ImageObserver observer,
-			int x, int y, float scale);
+    /**
+     * Draws the item on the slide.
+     * @param g the Graphics context
+     * @param observer the image observer
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param scale the scale factor
+     */
+    public abstract void draw(Graphics g, ImageObserver observer,
+            int x, int y, float scale);
 
-	/**
-	 * Gets the bounding box of the item.
-	 * @param g the Graphics context
-	 * @param observer the image observer
-	 * @param scale the scale factor
-	 * @return the bounding box Rectangle
-	 */
-	public abstract Rectangle getBoundingBox(Graphics g,
-			ImageObserver observer,
-			float scale);
+    /**
+     * Gets the bounding box of the item.
+     * @param g the Graphics context
+     * @param observer the image observer
+     * @param scale the scale factor
+     * @return the bounding box Rectangle
+     */
+    public abstract Rectangle getBoundingBox(Graphics g,
+            ImageObserver observer,
+            float scale);
 
-	/**
-	 * Gets the item's color.
-	 * @return the color
-	 */
-	public Color getColor() {
-		return color;
-	}
+    /**
+     * Gets the item's color.
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
 
-	/**
-	 * Sets the item's color.
-	 * @param color the new color
-	 */
-	public void setColor(final Color color) {
-		this.color = color;
-	}
+    /**
+     * Sets the item's color.
+     * @param colorParam the new color
+     */
+    public void setColor(final Color colorParam) {
+        this.color = colorParam;
+    }
 }
