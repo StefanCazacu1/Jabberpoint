71fbe4a1661ea2d6ff298efbb2643d0cff72d190
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2025-06-07 05:05:32 +0200

    fixed keycontroller violations

--- src/main/java/jabberpoint/KeyController.java
+++ src/main/java/jabberpoint/KeyController.java
@@ -8,46 +8,46 @@ import java.awt.event.KeyAdapter;
  */
 public class KeyController extends KeyAdapter {
 
-	/**
-	 * The Presentation this controller manipulates.
-	 */
-	private final Presentation presentation;
+    /**
+     * The Presentation this controller manipulates.
+     */
+    private final Presentation presentation;
 
-	/**
-	 * Constructs a KeyController for the given Presentation.
-	 *
-	 * @param presentationParam the Presentation to control
-	 */
-	public KeyController(final Presentation presentationParam) {
-		this.presentation = presentationParam;
-	}
+    /**
+     * Constructs a KeyController for the given Presentation.
+     *
+     * @param presentationParam the Presentation to control
+     */
+    public KeyController(final Presentation presentationParam) {
+        this.presentation = presentationParam;
+    }
 
-	/**
-	 * Handles key press events for slide navigation.
-	 *
-	 * @param keyEvent the KeyEvent to process
-	 */
-	@Override
-	public void keyPressed(final KeyEvent keyEvent) {
-		switch (keyEvent.getKeyCode()) {
-			case KeyEvent.VK_PAGE_DOWN:
-			case KeyEvent.VK_DOWN:
-			case KeyEvent.VK_RIGHT:
-				presentation.nextSlide();
-				break;
-			case KeyEvent.VK_PAGE_UP:
-			case KeyEvent.VK_UP:
-			case KeyEvent.VK_LEFT:
-				presentation.prevSlide();
-				break;
-			case KeyEvent.VK_Q:
-				System.exit(0);
-				break;
-			case KeyEvent.VK_HOME:
-				presentation.setSlideNumber(0);
-				break;
-			default:
-				// Do nothing
-		}
-	}
+    /**
+     * Handles key press events for slide navigation.
+     *
+     * @param keyEvent the KeyEvent to process
+     */
+    @Override
+    public void keyPressed(final KeyEvent keyEvent) {
+        switch (keyEvent.getKeyCode()) {
+            case KeyEvent.VK_PAGE_DOWN:
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_RIGHT:
+                presentation.nextSlide();
+                break;
+            case KeyEvent.VK_PAGE_UP:
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_LEFT:
+                presentation.prevSlide();
+                break;
+            case KeyEvent.VK_Q:
+                System.exit(0);
+                break;
+            case KeyEvent.VK_HOME:
+                presentation.setSlideNumber(0);
+                break;
+            default:
+                // Do nothing
+        }
+    }
 }
